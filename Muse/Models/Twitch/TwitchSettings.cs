29b0f27751b72a8319c01a27980fb8d8c8b35747
Elyas Page <elyaspage@googlemail.com>
2021-07-24 20:37:02 +0100

    Create TwitchSettings model

---
+++ Muse/Models/Twitch/TwitchSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Muse.Models.Twitch
+{
+    public class TwitchSettings
+    {
+        public string clientId { get; set; }
+        public string clientSecret { get; set; }
+        public string[] scope { get; set; }
+    }
+}
