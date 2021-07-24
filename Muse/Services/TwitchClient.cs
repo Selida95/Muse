0515017a3e29c4ed3a8088cfc20eda34ac06377e
Elyas Page <elyaspage@googlemail.com>
2021-07-24 21:26:28 +0100

    Create GetAppAccessToken method

--- Muse/Services/TwitchClient.cs
+++ Muse/Services/TwitchClient.cs
@@ -1,6 +1,9 @@
-﻿using Muse.Models.Twitch;
+﻿using Microsoft.Extensions.Options;
+using Muse.Models.Twitch;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -10,17 +13,19 @@ namespace Muse.Services
     public class TwitchClient
     {
         private HttpClient _client;
-        public TwitchClient(HttpClient client)
+        private TwitchSettings _settings;
+        public TwitchClient(HttpClient client, IOptions<TwitchSettings> settings)
         {
             _client = client;
+            _settings = settings.Value;
         }
 
-        public TokenResponse GetAppAccessToken()
+        public async Task<TokenResponse> GetAppAccessToken()
         {
             _client.BaseAddress = new Uri("https://id.twitch.tv");
-            _client.PostAsync("/oauth2/token?client_id=" + , new StringContent(""));
+            var response = await _client.PostAsync($"/oauth2/token?client_id={_settings.clientId}&client_secrets={_settings.clientSecret}&grant_type=client_credentials&{string.Join("%20", _settings.scope)}", new StringContent(""));
 
-            return new TokenResponse();
+            return JsonConvert.DeserializeObject<TokenResponse>(await response.Content.ReadAsStringAsync());
         }
     }
 }
