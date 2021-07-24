755f444b431a053337cf415faae5e4f8984c0376
Elyas Page <elyaspage@googlemail.com>
2021-07-24 14:28:25 +0100

    Add TokenResponse model

---
+++ Muse/Models/Twitch/TokenResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Muse.Models.Twitch
+{
+    public class TokenResponse
+    {
+        [JsonProperty("access_token")]
+        public string accessToken { get; set; }
+        [JsonProperty("refresh_token")]
+        public string? refreshToken { get; set; }
+        [JsonProperty("expires_in")]
+        public int expiresIn { get; set; }
+        [JsonProperty("scope")]
+        public string[] scope { get; set; }
+        [JsonProperty("token_type")]
+        public string tokenType { get; set; }
+    }
+}
